377aeb83cbe596c531a3b6fe79a0859e4bd53f9f
Michael Vilabrera <m.vilabrera@gmail.com>
2019-06-17 11:14:57 -0400

    Animating Views and Transitions

--- PlayingWithSwiftUI/SupportingViews/Badge/Badge.swift
+++ PlayingWithSwiftUI/SupportingViews/Badge/Badge.swift
@@ -14,8 +14,7 @@ struct Badge : View {
     var badgeSymbols: some View {
         ForEach(0..<Badge.rotationCount) { i in
             RotatedBadgeSymbol(angle: .degrees(Double(i) / Double(Badge.rotationCount)) * 360.0
-            )
-            }
+            )}
             .opacity(0.5)
     }
     
@@ -28,8 +27,8 @@ struct Badge : View {
                     .scaleEffect(1.0 / 4.0, anchor: .top)
                     .position(x: geometry.size.width / 2.0, y: (3.0 / 4.0) * geometry.size.height)
             }
-            }
-            .scaledToFit()
+        }
+        .scaledToFit()
     }
 }
 
---
+++ PlayingWithSwiftUI/SupportingViews/Hike/GraphCapsule.swift
@@ -0,0 +1,46 @@
+//
+//  GraphCapsule.swift
+//  PlayingWithSwiftUI
+//
+//  Created by Michael Vilabrera on 6/17/19.
+//  Copyright © 2019 Michael Vilabrera. All rights reserved.
+//
+
+import SwiftUI
+
+struct GraphCapsule: View {
+    var index: Int
+    var height: Length
+    var range: Range<Double>
+    var overallRange: Range<Double>
+    
+    var heightRatio: Length {
+        max(Length(magnitude(of: range) / magnitude(of: overallRange)), 0.15)
+    }
+    
+    var offsetRatio: Length {
+        Length((range.lowerBound - overallRange.lowerBound) / magnitude(of: overallRange))
+    }
+    
+    var animation: Animation {
+        Animation.spring(initialVelocity: 5)
+            .speed(2)
+            .delay(0.03 * Double(index))
+    }
+    
+    var body: some View {
+        Capsule()
+            .fill(Color.white)
+            .frame(height: height * heightRatio, alignment: .bottom)
+            .offset(x: 0, y: height * -offsetRatio)
+            .animation(animation)
+    }
+}
+
+#if DEBUG
+struct GraphCapsule_Previews: PreviewProvider {
+    static var previews: some View {
+        GraphCapsule(index: 0, height: 150, range: 10..<50, overallRange: 0..<100)
+    }
+}
+#endif
